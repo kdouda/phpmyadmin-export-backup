5daf96022bf448b3aefea86b76eefb3178fbc41a
Karel Douda <karel.douda@memos.cz>
2021-02-28 15:58:55 +0100

    Restructure some code

--- index.js
+++ index.js
@@ -1,5 +1,96 @@
 const puppeteer = require('puppeteer');
 const credentials = require('./credentials.json');
+const path = require('path');
+
+(async () => {
+     // { headless: false} as a parameter is useful for troubleshooting, or just for some fun
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+  
+    // login
+  
+    await fnLogin(page);
+    await wait(3000);
+  
+    // since PMA is a great and modern application, it works with frames!
+    const frameHandle = await page.$('#frame_content');
+    const frame = await frameHandle.contentFrame();
+  
+    // locating the DB list page
+    await fnDbListPage(frame);
+    
+    // locating the database's page
+    await wait(3000);
+    await fnDatabaseDetailPage(frame);
+    
+    // locating the export page
+    await fnExportPage(frame);
+  
+    // exporting with default options
+    await fnDownload(frame, page);
+    
+    // and we are done
+    await browser.close();
+})();
+
+const fnLogin = async (page) => {
+    // navigate to PMA page
+    await page.goto(credentials.pma);
+
+    await page.type('#input_username', credentials.username);
+    await page.type('#input_password', credentials.password);
+    
+    await Promise.all([
+      page.click('#input_go'),
+      page.waitForNavigation(),
+    ]);
+};
+
+const fnDbListPage = async (frame) => {
+    // looking for the link to the list of databases
+    const dbLinks = await frame.$x("//a[contains(@href, 'server_databases.php')]");
+
+    await Promise.all([
+      dbLinks[0].click(),
+      frame.waitForNavigation(),
+    ]);
+};
+
+const fnDatabaseDetailPage = async (frame) => {
+    // looking for the link to the database link - exact match
+    const dbLinks2 = await frame.$x("//a[contains(., '" + credentials.database + "')]");
+
+    await Promise.all([
+      dbLinks2[0].click(),
+      frame.waitForNavigation(),
+    ]);
+};
+
+const fnExportPage = async (frame) => {
+    // looking for the link to the export button, the href link contains the session id...
+    const dbExportLink = await frame.$x("//a[contains(@href, 'db_export.php')]");
+  
+    await Promise.all([
+      dbExportLink[0].click(),
+      frame.waitForNavigation({waitUntil: "domcontentloaded"}),
+    ]);
+};
+
+const fnDownload = async (frame, page) => {
+    await frame.waitForSelector('#buttonGo'); // the export button itself
+
+    await page._client.send(
+      'Page.setDownloadBehavior',
+      {
+        behavior: 'allow',
+        downloadPath: path.resolve(__dirname, 'exports'),
+      },
+    );
+  
+    await frame.click('#buttonGo');
+  
+    await wait(30000); // default PHP max_execution_time, PMA usually cannot exceed this?
+};
 
 const wait = (len) => {
     len = len || 10000;
@@ -10,73 +101,3 @@ const wait = (len) => {
         }, len);
     });
 };
-
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  const path = require('path')
-
-  // login
-
-  await page.goto(credentials.pma);
-
-  await page.type('#input_username', credentials.username);
-  await page.type('#input_password', credentials.password);
-  
-  await Promise.all([
-    page.click('#input_go'),
-    page.waitForNavigation(),
-  ]);
-
-  // locating the DB list page
-
-  await wait(3000);
-
-  const frameHandle = await page.$('#frame_content');
-  const frame = await frameHandle.contentFrame();
-
-  const dbLinks = await frame.$x("//a[contains(@href, 'server_databases.php')]");
-
-  await Promise.all([
-    dbLinks[0].click(),
-    frame.waitForNavigation(),
-  ]);
-  
-  // locating the database's page
-
-  await wait(3000);
-
-  const dbLinks2 = await frame.$x("//a[contains(., '" + credentials.database + "')]");
-
-  await Promise.all([
-    dbLinks2[0].click(),
-    frame.waitForNavigation(),
-  ]);
-
-  // locating the export page
-
-  const dbExportLink = await frame.$x("//a[contains(@href, 'db_export.php')]");
-  
-  await Promise.all([
-    dbExportLink[0].click(),
-    frame.waitForNavigation({waitUntil: "domcontentloaded"}),
-  ]);
-
-  // exporting with default options
-
-  await frame.waitForSelector('#buttonGo');
-
-  await page._client.send(
-    'Page.setDownloadBehavior',
-    {
-      behavior: 'allow',
-      downloadPath: path.resolve(__dirname, 'exports'),
-    },
-  );
-
-  await frame.click('#buttonGo');
-
-  await wait(30000); // default PHP max_execution_time, PMA usually cannot exceed this?
-
-  await browser.close();
-})();
